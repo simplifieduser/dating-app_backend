8b9a132eb2b222803af4d0bceae83f70d318c588
Jonah Zabel <develop.polony@gmx.net>
2024-09-13 10:49:28 +0200

    Update game manager

--- src/mgn.ts
+++ src/mgn.ts
@@ -4,9 +4,13 @@ import { Server } from "socket.io";
 type GameState = {[gid: UUID] : {
   players: {[uid: UUID] : {
     name?: string,
-    connected: boolean 
+    connected: boolean,
+    admin: boolean
   }},
-  game: {}
+  game: {
+    state: "lobby" | "game"
+    mode: "default"
+  }
 }}
 
 
@@ -29,11 +33,15 @@ export class GameManager {
   createNewGame(): UUID {
 
     const gid = randomUUID();
+    let firstJoin = true;
 
     // Add game to state
     this.#state[gid] = {
       players: {},
-      game: {}
+      game: {
+        state: "lobby",
+        mode: "default"
+      }
     }
 
     // Create new namespace for game sockets
@@ -42,18 +50,19 @@ export class GameManager {
     // Register connect event
     nsp.on("connection", (socket) => {
 
-      // Register join event
-      socket.on("join", (data: { uid?: UUID, name?: string }, res) => {
+      // Register join event, only callable once per connection attempt
+      socket.once("join", (data: { uid?: UUID, name?: string }, res) => {
 
         const uid = data.uid || randomUUID()
 
-        // Add/Set player status
+        // Add/Set player status - if first join set admin
         this.#state[gid].players[uid] = {
           name: data.name,
-          connected: true
+          connected: true,
+          admin: firstJoin
         }
 
-        // Add socket to its own room (for individual communcication)
+        // Add socket to its own room (for individual communication)
         socket.join(uid)
 
         // Register disconnect event
@@ -62,6 +71,12 @@ export class GameManager {
           // Set connection status of player, if exist
           if (this.#state[gid].players[uid] !== undefined)
             this.#state[gid].players[uid].connected = false
+
+          // If admin disconnects close game
+          if (this.#state[gid].players[uid].admin) {
+            nsp.disconnectSockets()
+            delete this.#state[gid]
+          }
 
         })
 
@@ -76,17 +91,31 @@ export class GameManager {
 
         })
 
-        // Acknoledge uid
-        res({ uid: uid })
+        // if admin, register change game mode event
+        if (firstJoin) {
+          socket.on("change_mode", (newMode) => {
+            //TODO: CHeck if newMode is valid
+            this.#state[gid].game.mode = newMode
+            nsp.emit("state_changed", this.#state[gid])
+          })
+        }
+
+        // if admin, register start game event
+        if (firstJoin) {
+          socket.on("start_game", () => {
+            this.#state[gid].game.state = "game"
+            nsp.emit("state_changed", this.#state[gid])
+          })
+        }
+
+        // Return info
+        res({ uid: uid, admin: firstJoin })
+
+        firstJoin = false;
 
       })
 
     })
-
-    // Register auto-close event
-    setInterval(() => {
-      
-    }, 3600)
 
     return gid
 
